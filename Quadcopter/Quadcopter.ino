42734136bb74bd54bdd87f923f77e5dd85336367
Subhankar Pal <subhankarpal@Subhankars-MacBook-Pro.local>
2015-02-09 22:42:12 +0530

    Cleaned up the code

--- Quadcopter/Quadcopter.ino
+++ Quadcopter/Quadcopter.ino
@@ -2,7 +2,8 @@
 #include <SoftwareSerial.h>// import the serial library
 
 SoftwareSerial bluetooth (10, 11);
-double mykp, myki, mykd;
+double myKp, myKi, myKd;
+float deltaK = 0.01;
 
 extern unsigned long lastTime, sampleTime;
 extern double input, output, setPoint, errorSum, ITerm, lastInput, kp, ki, kd, outMin, outMax;
@@ -11,12 +12,12 @@ extern int controllerDirection, PIDMode;
 void setup() {
   // Set PID controls.
   setPIDMode (ENABLED);
-  mykp = .2;
-  myki = 1.2;
-  mykd = .02;
-  setTuningParameters (mykp, myki, mykd);
+  myKp = .2;
+  myKi = 1.2;
+  myKd = .02;
+  setTuningParameters (myKp, myKi, myKd);
   setOutputLimits (0, 255);
-  setSampleTime (10);
+  setSampleTime (1000);
   
   bluetooth.begin(9600);
   bluetooth.println("Bluetooth On: please press capital letter to increase or small letter to decrease parameter value");
@@ -33,33 +34,41 @@ void loop() {
   pollParametersToTune ();
   compute ();
   analogWrite (9, int (output));
-  delay(500);
+  delay(250);
 }
 
 void pollParametersToTune() {
-   if (bluetooth.available()) {
-     int bluetoothData = bluetooth.read();
-     if ( bluetoothData == 'P') {
-       mykp += .05;
-     }
-     else if (bluetoothData == 'p') {// if number 0 pressed ....
-       mykp -= .05;
-       if (mykp < 0) mykp = 0;
-     }
-     else if ( bluetoothData == 'I') {
-       myki += .05;
-     }
-     else if (bluetoothData == 'i') {// if number 0 pressed ....
-       myki -= .05;
-       if (myki < 0) myki = 0;
-     }
-     else if ( bluetoothData == 'D') {
-       mykd += .05;
-     }
-     else if (bluetoothData == 'd') {// if number 0 pressed ....
-       mykd -= .05;
-       if (mykd < 0) mykd = 0;
-     }
-     setTuningParameters (mykp, myki, mykd);
-   }
+  if (bluetooth.available()) {
+    int bluetoothData = bluetooth.read();
+    switch (bluetoothData) {
+      case 'P':
+        myKp += deltaK;
+        break;
+      case 'p':
+        myKp -= deltaK;
+        if (myKp < 0) {
+          myKp = 0.0;
+        }
+        break;
+      case 'I':
+        myKi += deltaK;
+        break;
+      case 'i':
+        myKi -= deltaK;
+        if (myKi < 0) {
+          myKi = 0.0;
+        }
+        break;
+      case 'D':
+        myKd += deltaK;
+        break;
+      case 'd':
+        myKd -= deltaK;
+        if (myKd < 0) {
+          myKd = 0.0;
+        }
+        break;
+    }
+    setTuningParameters (myKp, myKi, myKd);
+  }
 }
