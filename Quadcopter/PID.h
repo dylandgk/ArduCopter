8e00e986325a8fecd5867f94792246b29f9c4af0
Subhankar Pal <subhankarpal@Subhankars-MacBook-Pro.local>
2015-02-09 20:41:54 +0530

    Added support to tune parameters via bluetooth (HC-05)

---
+++ Quadcopter/PID.h
@@ -0,0 +1,135 @@
+#include <arduino.h>
+
+unsigned long lastTime = 0;
+unsigned long sampleTime = 1000;      // 1 second.
+
+double input, output, setPoint;
+double errorSum, ITerm, lastInput;
+double kp, ki, kd;
+
+#define ENABLED   1
+#define DISABLED  0
+
+#define DIRECT    0
+#define REVERSE   1
+int controllerDirection = DIRECT;
+
+int PIDMode = DISABLED;
+
+double outMin, outMax;
+
+void compute () {
+  if (!PIDMode) {
+    return;
+  }
+  unsigned long currentTime = millis ();
+  unsigned int timeDifference = currentTime - lastTime;
+  
+  if (timeDifference >= sampleTime) {
+    double error = setPoint - input;
+    // Compute all the working error variables.
+    ITerm += ki * error;
+    
+    // Clamp the integral term.
+    if (ITerm > outMax) {
+      ITerm = outMax;
+    }
+    else if (ITerm < outMin) {
+      ITerm = outMin;
+    }
+    
+    double inputDifference = input - lastInput;
+    
+    // Store some variables in the memory for the next call of this function.
+    lastTime = currentTime;
+    lastInput = input;
+    
+    // Compute PID output.
+    output = (kp * error) + (ITerm) - (kd * inputDifference);
+    
+    // Clamp the output.
+    if (output > outMax) {
+      output = outMax;
+    }
+    else if (output < outMin) {
+      output = outMin;
+    }
+  }
+}
+
+void setTuningParameters (double Kp, double Ki, double Kd) {
+  if (Kp < 0 || Ki < 0 || Kd < 0) {
+    return;
+  }
+  double sampleTimeInSec = ((double) sampleTime) / 1000;
+  kp = Kp;
+  ki = Ki * sampleTimeInSec;
+  kd = Kd / sampleTimeInSec;
+  if (controllerDirection == REVERSE) {
+    kp = -kp;
+    ki = -ki;
+    kd = -kd;
+  }
+  Serial.print (" Kp: ");
+  Serial.print (Kp);
+  Serial.print (" Ki: ");
+  Serial.print (Ki);
+  Serial.print (" Kd: ");
+  Serial.println (Kd);
+}
+
+void setSampleTime (unsigned long newSampleTime) {
+  if (newSampleTime > 0) {
+    sampleTime = newSampleTime;
+    double sampleTimeRatio = newSampleTime / sampleTime;
+    ki = ki * sampleTimeRatio;
+    kd = kd / sampleTimeRatio;
+  }
+}
+
+void setOutputLimits (double minVal, double maxVal) {
+  if (maxVal < minVal) {
+    return;
+  }
+  outMax = maxVal;
+  outMin = minVal;
+  
+  if (output > maxVal) {
+    output = maxVal;
+  }
+  else if (output < minVal) {
+    output = minVal;
+  }
+  
+  if (ITerm > maxVal) {
+    ITerm = maxVal;
+  }
+  else if (ITerm < minVal) {
+    ITerm = minVal;
+  }
+}
+
+void initialize () {
+  lastInput = input;
+  ITerm = output;
+  if (ITerm > outMax) {
+    ITerm = outMax;
+  }
+  else if (ITerm < outMin) {
+    ITerm = outMin;
+  }
+}
+
+void setPIDMode (int mode) {
+  int newPIDMode = (mode == ENABLED);
+  if (newPIDMode && !(PIDMode)) {
+    initialize ();
+  }
+  PIDMode = newPIDMode;
+}
+
+void setControllerDirection (int dir) {
+  controllerDirection = dir;
+}
+
+  
--- Quadcopter/Quadcopter.ino
+++ Quadcopter/Quadcopter.ino
@@ -1,11 +1,21 @@
+#include "PID.h"
+#include <SoftwareSerial.h>// import the serial library
+
+SoftwareSerial bluetooth (10, 11);
+double mykp, myki, mykd;
+
 void setup() {
   // Set PID controls.
   setPIDMode (ENABLED);
-  setTuningParameters (.2, 1.2, .02);
+  mykp = .2;
+  myki = 1.2;
+  mykd = .02;
+  setTuningParameters (mykp, myki, mykd);
   setOutputLimits (0, 255);
   setSampleTime (10);
-  setControllerDirection (REVERSE);
   
+  bluetooth.begin(9600);
+  bluetooth.println("Bluetooth On: please press capital letter to increase or small letter to decrease parameter value");
   // Print on serial monitor.
   Serial.begin(9600);
   setPoint = 60;
@@ -16,7 +26,36 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   input = analogRead(A1);
+  pollParametersToTune ();
   compute ();
   analogWrite (9, int (output));
-  delay(5);
+  delay(500);
+}
+
+void pollParametersToTune() {
+   if (bluetooth.available()) {
+     int bluetoothData = bluetooth.read();
+     if ( bluetoothData == 'P') {
+       mykp += .05;
+     }
+     else if (bluetoothData == 'p') {// if number 0 pressed ....
+       mykp -= .05;
+       if (mykp < 0) mykp = 0;
+     }
+     else if ( bluetoothData == 'I') {
+       myki += .05;
+     }
+     else if (bluetoothData == 'i') {// if number 0 pressed ....
+       myki -= .05;
+       if (myki < 0) myki = 0;
+     }
+     else if ( bluetoothData == 'D') {
+       mykd += .05;
+     }
+     else if (bluetoothData == 'd') {// if number 0 pressed ....
+       mykd -= .05;
+       if (mykd < 0) mykd = 0;
+     }
+     setTuningParameters (mykp, myki, mykd);
+   }
 }
